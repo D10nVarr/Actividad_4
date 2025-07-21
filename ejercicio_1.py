3f45a0177cb38ddfb9507785c31901e0b01cbf12
diegobarr <d1390xd.v@gmail.com>
2025-07-21 10:19:32 -0600

    Codigo inicial con error en condicional 1

--- ejercicio_1.py
+++ ejercicio_1.py
@@ -1,1 +1,10 @@
-full_name=input("Por favor ingrese su nombre completo: ")
+full_name=input("Por favor ingrese su nombre completo: ").lower()
+dpi_number=int(input("Ingrese su número de dpi: "))
+name_department=input("Ingrese su departamento: ").lower()
+year_birth=int(input("Ingrese su año de nacimiento"))
+
+if len(dpi_number)<13 or len(dpi_number)>13:
+    print("Número de DPI incorrecto")
+
+elif year_birth>2007 and name_department !="alta verapaz" or name_department =="petén":
+    print("Usted es menor de edad, no puede votar")
