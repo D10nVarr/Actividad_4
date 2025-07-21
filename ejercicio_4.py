cab879769f53d3e65e31e97b1a7a3719660b3204
diegobarr <d1390xd.v@gmail.com>
2025-07-21 17:28:37 -0600

    Factura final con sus cálculos correspondientes

--- ejercicio_4.py
+++ ejercicio_4.py
@@ -1,10 +1,10 @@
 num_prices=int(input("Ingrese cantidad de precios que deseee agregar: "))
-total_price=0
+sub_price=0
 
 for i in range(num_prices):
 
     any_price=int(input(f"Ingrese el precio del producto {i+1}: "))
-    total_price+=any_price
+    sub_price+=any_price
 
 amount_tip=input("Desea dejar popina, escoja: si o no: ").lower()
 if amount_tip=="si":
@@ -13,11 +13,29 @@ if amount_tip=="si":
 elif amount_tip=="no":
     amount_tip=0
 
-client_tarjet=input("Posee tarjeta de cliente frecuente, escoja: si o no: ").lower()
-if client_tarjet=="si":
-    client_tarjet=0.1
-elif client_tarjet=="no":
-    client_tarjet=0
+client_member=input("Posee tarjeta de cliente frecuente, escoja: si o no: ").lower()
+if client_member=="si":
+    client_member=0.1
+elif client_member=="no":
+    client_member=0
+
+iva_tax=sub_price*(0.12)
+tip=sub_price*amount_tip
+discount=sub_price*client_member
+
+total_price=sub_price+iva_tax+tip-discount
+
+print("Su factura dettallada es:")
+print("Subtotal   ", sub_price)
+print("+ IVA      ", iva_tax)
+print("+ Propina  ", tip)
+print("- Descuento", discount)
+print("-------------------------------------")
+print("TOTAL:     ", total_price)
+
+
+
+
 
 
 
