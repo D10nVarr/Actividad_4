420afba748522fcf78c418d4816c6a492fda46c2
diegobarr <d1390xd.v@gmail.com>
2025-07-21 18:52:43 -0600

    Commit inicial ejercicio 6

---
+++ ejercicio_6.py
@@ -0,0 +1,7 @@
+package_weight=int(input("Ingrese en kg el peso del paquete: "))
+delivery_distance=float(input("Ingrese en km la distancia de entrega: "))
+urgent_package=input("¿El paquete es urgente? si/no: ").lower()
+package_size=input("Ingrese el tamaño del paquete (pequeño/mediano/grande): ").lower()
+
+urgency_surcharge=0
+size_surcharge=0
