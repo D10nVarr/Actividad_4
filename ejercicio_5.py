42b31fc81a0d1d81c2e3d6ed21b1e54909005f14
diegobarr <d1390xd.v@gmail.com>
2025-07-21 18:19:08 -0600

    Prueba de ley de seller

--- ejercicio_5.py
+++ ejercicio_5.py
@@ -1,11 +1,41 @@
-any_date=input("Ingrese una fecha: ").split("/")
+inicial_date=input("Ingrese una fecha: ")
+any_date=inicial_date.split("/")
 
-if (any_date[0]<0 or any_date[0]>31) or (any_date[1]<0 or any_date[1]>12) or (any_date[2]<0):
+day=int(any_date[0])
+month=int(any_date[1])
+year=int(any_date[2])
+
+century=year//100
+year_century=year-(century*100)
+
+if (day<0 or day>31) or (month<0 or month>12) or (year<0):
     print("La fecha no es válida")
 
-elif any_date[1]==2:
-    if any_date[2]%4==0:
-        if any_date[0]<=29:
-            print("Fecha válida")
+elif month==2:
+    if year%4==0:
+        if day<=29:
+            zeller=(day+((13(month+1))/5)+year_century+(year_century/4)+(century/4)+5(century))%7
+
+            if zeller==0:
+                print(f"La fecha {inicial_date} fue un sábado")
+            elif zeller==1:
+                print(f"La fecha {inicial_date} fue un domingo")
+            elif zeller==1:
+                print(f"La fecha {inicial_date} fue un domingo")
+            elif zeller==1:
+                print(f"La fecha {inicial_date} fue un domingo")
+            elif zeller==1:
+                print(f"La fecha {inicial_date} fue un domingo")
+            elif zeller==1:
+                print(f"La fecha {inicial_date} fue un domingo")
+            elif zeller==1:
+                print(f"La fecha {inicial_date} fue un domingo")
+
+        else:
+            print("La fecha no es válida")
     else:
         print("La fecha no es válida")
+
+elif month==1 or month==3 or month==5 or month==7 or month==8 or month==10:
+    if day<=31:
+        print()
