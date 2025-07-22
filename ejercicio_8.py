de515693099547b850df7adb983140cdada76dbe
diegobarr <d1390xd.v@gmail.com>
2025-07-21 21:17:41 -0600

    Definicion inicial de las coordenadas

---
+++ ejercicio_8.py
@@ -0,0 +1,6 @@
+cord_a=input("Ingrese la primera coordenada (norte, sur, este, oeste): ").lower()
+cord_b=input("Ingrese la segunda coordenada (norte, sur, este, oeste): ").lower()
+
+coords=["norte","sur","este","oeste"]
+
+
