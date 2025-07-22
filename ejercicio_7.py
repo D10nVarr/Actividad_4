748f6975fec5656a7856d9feff1cf51919a04c5d
diegobarr <d1390xd.v@gmail.com>
2025-07-21 20:50:56 -0600

    Bucle con curva funcional, punto de retorno

--- ejercicio_7.py
+++ ejercicio_7.py
@@ -12,4 +12,20 @@ for i in range(5):
     students_notes.append(note_2)
     students_notes.append(note_3)
 
-if
+average_1=(students_notes[0]+students_notes[1]+students_notes[2])/3
+average_2=(students_notes[3]+students_notes[4]+students_notes[5])/3
+average_3=(students_notes[6]+students_notes[7]+students_notes[8])/3
+average_4=(students_notes[9]+students_notes[10]+students_notes[11])/3
+average_5=(students_notes[12]+students_notes[13]+students_notes[14])/3
+
+if average_1<70 and average_2<70 and average_3<70 and average_4<70 and average_5<70:
+    print("Los estudiantes aplican a curva")
+
+    for i in range(15):
+        if (students_notes[i])<=95:
+            students_notes[i]+=5
+        elif (students_notes[i])>95:
+            students_notes[i]=100
+
+
+
