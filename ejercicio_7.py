1c27c19289c7f9f2b79ccc159e4dfcbfefa00f91
diegobarr <d1390xd.v@gmail.com>
2025-07-21 21:04:16 -0600

    Codigo que si funciona

--- ejercicio_7.py
+++ ejercicio_7.py
@@ -27,5 +27,22 @@ if average_1<70 and average_2<70 and average_3<70 and average_4<70 and average_5
         elif (students_notes[i])>95:
             students_notes[i]=100
 
+    new_average_1=(students_notes[0]+students_notes[1]+students_notes[2])/3
+    new_average_2=(students_notes[3]+students_notes[4]+students_notes[5])/3
+    new_average_3=(students_notes[6]+students_notes[7]+students_notes[8])/3
+    new_average_4=(students_notes[9]+students_notes[10]+students_notes[11])/3
+    new_average_5=(students_notes[12]+students_notes[13]+students_notes[14])/3
+
+    total_average=[new_average_1,new_average_2,new_average_3,new_average_4,new_average_5]
+
+    for i in range(5):
+        print(f"Nombre del estudiante {students_name[i]}        Promedio: {total_average[i]}")
+
+else:
+    total_average = [average_1, average_2, average_3, average_4, average_5]
+    for i in range(5):
+        print(f"Nombre del estudiante {students_name[i]}        Promedio: {total_average[i]}")
+
+
 
 
