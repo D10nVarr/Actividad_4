23d102da8e8c19d0a222876566d3dc0ee64ed689
diegobarr <d1390xd.v@gmail.com>
2025-07-21 18:29:35 -0600

    Código arreglado

--- ejercicio_5.py
+++ ejercicio_5.py
@@ -14,28 +14,63 @@ if (day<0 or day>31) or (month<0 or month>12) or (year<0):
 elif month==2:
     if year%4==0:
         if day<=29:
-            zeller=(day+((13(month+1))/5)+year_century+(year_century/4)+(century/4)+5(century))%7
+            zeller=(day+((13*(month+1))//5)+year_century+(year_century//4)+(century//4)+5*(century))%7
 
             if zeller==0:
                 print(f"La fecha {inicial_date} fue un sábado")
             elif zeller==1:
                 print(f"La fecha {inicial_date} fue un domingo")
-            elif zeller==1:
-                print(f"La fecha {inicial_date} fue un domingo")
-            elif zeller==1:
-                print(f"La fecha {inicial_date} fue un domingo")
-            elif zeller==1:
-                print(f"La fecha {inicial_date} fue un domingo")
-            elif zeller==1:
-                print(f"La fecha {inicial_date} fue un domingo")
-            elif zeller==1:
-                print(f"La fecha {inicial_date} fue un domingo")
-
+            elif zeller==2:
+                print(f"La fecha {inicial_date} fue un lunes")
+            elif zeller==3:
+                print(f"La fecha {inicial_date} fue un martes")
+            elif zeller==4:
+                print(f"La fecha {inicial_date} fue un miércoles")
+            elif zeller==5:
+                print(f"La fecha {inicial_date} fue un jueves")
+            elif zeller==6:
+                print(f"La fecha {inicial_date} fue un viernes")
         else:
             print("La fecha no es válida")
     else:
         print("La fecha no es válida")
 
-elif month==1 or month==3 or month==5 or month==7 or month==8 or month==10:
+elif month==1 or month==3 or month==5 or month==7 or month==8 or month==10 or month==12:
     if day<=31:
-        print()
+        zeller=(day+((13*(month+1))//5)+year_century+(year_century//4)+(century//4)+5*(century))%7
+        if zeller == 0:
+            print(f"La fecha {inicial_date} fue un sábado")
+        elif zeller == 1:
+            print(f"La fecha {inicial_date} fue un domingo")
+        elif zeller == 2:
+            print(f"La fecha {inicial_date} fue un lunes")
+        elif zeller == 3:
+            print(f"La fecha {inicial_date} fue un martes")
+        elif zeller == 4:
+            print(f"La fecha {inicial_date} fue un miércoles")
+        elif zeller == 5:
+            print(f"La fecha {inicial_date} fue un jueves")
+        elif zeller == 6:
+            print(f"La fecha {inicial_date} fue un viernes")
+    else:
+        print("La fecha no es válida")
+
+elif month==4 or month==6 or month==9 or month==11:
+    if day<=30:
+        zeller=(day+((13*(month+1))//5)+year_century+(year_century//4)+(century//4)+5*(century))%7
+        if zeller == 0:
+            print(f"La fecha {inicial_date} fue un sábado")
+        elif zeller == 1:
+            print(f"La fecha {inicial_date} fue un domingo")
+        elif zeller == 2:
+            print(f"La fecha {inicial_date} fue un lunes")
+        elif zeller == 3:
+            print(f"La fecha {inicial_date} fue un martes")
+        elif zeller == 4:
+            print(f"La fecha {inicial_date} fue un miércoles")
+        elif zeller == 5:
+            print(f"La fecha {inicial_date} fue un jueves")
+        elif zeller == 6:
+            print(f"La fecha {inicial_date} fue un viernes")
+    else:
+        print("La fecha no es válida")
