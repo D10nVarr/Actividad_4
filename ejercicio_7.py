90768cb5c576381d54fe255c8e6fd83860aff185
diegobarr <d1390xd.v@gmail.com>
2025-07-21 20:23:00 -0600

    Agregado de estudiantes y sus notas

--- ejercicio_7.py
+++ ejercicio_7.py
@@ -3,3 +3,13 @@ students_notes=[]
 
 for i in range(5):
     name=input(f"Ingrese el nombre del estudiante {i+1}: ")
+    note_1=int(input(f"Ingrese el nota 1: "))
+    note_2 = int(input(f"Ingrese el nota 2: "))
+    note_3 = int(input(f"Ingrese el nota 3: "))
+
+    students_name.append(name)
+    students_notes.append(note_1)
+    students_notes.append(note_2)
+    students_notes.append(note_3)
+
+if
