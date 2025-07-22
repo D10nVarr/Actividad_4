18dfdec1bce3d2a1bd2fc8dab0d7ed6df3ed4fab
diegobarr <d1390xd.v@gmail.com>
2025-07-21 22:41:57 -0600

    Verificador de fecha mayor y cercania correcto

--- ejercicio_10.py
+++ ejercicio_10.py
@@ -1,7 +1,7 @@
 date_1=input("Ingrese una fecha en formato dd/mm/yyyy: ")
 date_a=date_1.split("/")
 date_2=input("Ingrese otra fecha en formato dd/mm/yyyy: ")
-date_b=date_1.split("/")
+date_b=date_2.split("/")
 
 day=int(date_a[0])
 month=int(date_a[1])
@@ -11,20 +11,24 @@ day_b=int(date_b[0])
 month_b=int(date_b[1])
 year_b=int(date_b[2])
 
+if ((day<0 or day>31) or (month<0 or month>12) or (year<0)) or (day_b<0) or (month_b<0) or (year_b<0):
+    print("La fecha no es válida")
 
 #verificación de la fecha mayor
-if year>year_b:
+elif year>year_b:
     print(f"La fecha mayor es {date_1}")
 elif year<year_b:
     print(f"La fecha mayor es {date_2}")
 elif year==year_b:
     if month > month_b:
-        print(f"La fecha mayor es {date_1}")
+        print(f"La fecha mayor es {date_1} y esta en el mismo año que {date_2}")
     elif month < month_b:
         print(f"La fecha mayor es {date_2}")
     elif month == month_b:
         if day > day_b:
-            print(f"La fecha mayor es {date_1}")
+            print(f"La fecha mayor es {date_1} y esta en el mismo mes y año que {date_2}")
         elif day < day_b:
             print(f"La fecha mayor es {date_2}")
+        elif day == day_b:
+            print(f"Son la misma fecha")
 
